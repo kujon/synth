3d2007d18b7ddacc429c2b3ba15a995fe74bc235
kujon <jakub.korzeniowski@vortexa.com>
2022-05-15 16:35:48 +0100

    feat: testing whether rodio can successfully play soviet anthem

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use::std::fs::File;
+use::std::io::BufReader;
+use::rodio::{Decoder, OutputStream, source::Source};
+
 fn main() {
-    println!("Hello, world!");
+    let (stream, stream_handle) = OutputStream::try_default().unwrap();
+
+    let file = BufReader::new(File::open("/Users/jakubkorzeniowski/Downloads/Soviet_Anthem_Instrumental_1955.ogg").unwrap());
+
+    let source = Decoder::new(file).unwrap();
+
+    stream_handle.play_raw(source.convert_samples());
+
+    // The sound plays in a separate audio thread,
+    // so we need to keep the main thread alive while it's playing.
+    std::thread::sleep(std::time::Duration::from_secs(5));
 }
