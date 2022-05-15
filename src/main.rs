ef4c8663709c9a17855d7cfb25c35412191498fb
kujon <jakub.korzeniowski@vortexa.com>
2022-05-15 16:50:03 +0100

    chore: no unwraps

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,19 @@ use::std::fs::File;
 use::std::io::BufReader;
 use::rodio::{Decoder, OutputStream, source::Source};
 
-fn main() {
-    let (stream, stream_handle) = OutputStream::try_default().unwrap();
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (stream, stream_handle) = OutputStream::try_default()?;
 
-    let file = BufReader::new(File::open("/Users/jakubkorzeniowski/Downloads/Soviet_Anthem_Instrumental_1955.ogg").unwrap());
+    let file = File::open("/Users/jakubkorzeniowski/Downloads/Soviet_Anthem_Instrumental_1955.ogg")?;
+    let file_handle = BufReader::new(file);
 
-    let source = Decoder::new(file).unwrap();
+    let source = Decoder::new(file_handle)?;
 
-    stream_handle.play_raw(source.convert_samples());
+    let _ = stream_handle.play_raw(source.convert_samples())?;
 
     // The sound plays in a separate audio thread,
     // so we need to keep the main thread alive while it's playing.
     std::thread::sleep(std::time::Duration::from_secs(5));
+
+    Ok(())
 }
