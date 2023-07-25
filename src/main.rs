7b8b202608c77028740112de1b224265da35e1af
kujon <jakub.korzeniowski@gmail.com>
2023-07-25 11:41:34 +0100

    chore: bit of abstraction to hide crazy code + nicer debug trait

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod input;
 mod oscilators;
 mod utils;
+mod voices;
 
 use std::cmp::Ordering;
 use std::collections::{BTreeMap, VecDeque};
@@ -9,88 +10,29 @@ use std::time::SystemTime;
 use ::rodio::{OutputStream, Sink};
 // TO DO: replace this crate with proper windowing so that we don't have to grant accessibility permissions to listen to keystrokes globally.
 use rdev::{listen, Event, EventType, Key};
+use voices::voices::Voices;
 
 use crate::input::keys::ToNote;
 use crate::oscilators::sine::SineWave;
 use crate::oscilators::square::SquareWave;
 use crate::utils::frequency::Note;
 
-#[derive(Debug, Clone, Copy, Eq)]
-struct WithCreationTime<T> {
-    item: T,
-    creation_time: SystemTime,
-}
-
-impl<T> WithCreationTime<T> {
-    fn new(item: T) -> Self {
-        Self {
-            item,
-            creation_time: SystemTime::now(),
-        }
-    }
-}
-
-impl<T> PartialEq for WithCreationTime<T>
-where
-    T: PartialEq,
-{
-    fn eq(&self, other: &Self) -> bool {
-        self.item.eq(&other.item)
-    }
-}
-
-impl<T> PartialOrd for WithCreationTime<T>
-where
-    T: PartialEq + Eq,
-{
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        if self.item == other.item {
-            Some(Ordering::Equal)
-        } else {
-            self.creation_time.partial_cmp(&other.creation_time)
-        }
-    }
-}
-
-impl<T> Ord for WithCreationTime<T>
-where
-    T: PartialEq + Eq,
-{
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        if self.item == other.item {
-            Ordering::Equal
-        } else {
-            self.creation_time.cmp(&other.creation_time)
-        }
-    }
-}
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (_stream, stream_handle) = OutputStream::try_default()?;
     let num_voices = 4;
-    let mut voices: BTreeMap<WithCreationTime<Key>, Sink> = BTreeMap::new();
+    let mut voices = Voices::new(num_voices);
 
     // This will block.
     if let Err(error) = listen(move |event| match event.event_type {
         EventType::KeyPress(key) => {
-            if !voices.contains_key(&WithCreationTime::new(key)) {
+            if !voices.contains(&key) {
                 if let Some(note) = key.to_note() {
                     match Sink::try_new(&stream_handle) {
                         Ok(sink) => {
-                            println!("Note pressed {:?}", note);
                             sink.append(SineWave::new(note.to_frequency()));
                             sink.play();
-                            if voices.len() == num_voices {
-                                voices.pop_first();
-                                println!("pop");
-                                let just_notes: Vec<_> = voices.iter().map(|v| v.0.item).collect();
-                                println!("{:?}", just_notes);
-                            }
-                            voices.insert(WithCreationTime::new(key), sink);
-
-                            println!("start");
-                            let just_notes: Vec<_> = voices.iter().map(|v| v.0.item).collect();
-                            println!("{:?}", just_notes);
+                            voices.insert(key, sink);
+                            println!("Key pressed: {:?}, voices: {:?}", key, voices);
                         }
                         Err(e) => {
                             println!("{:?}", e);
@@ -100,11 +42,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
         }
         EventType::KeyRelease(key) => {
-            let removed = voices.remove(&WithCreationTime::new(key));
+            let removed = voices.remove(&key);
             if removed.is_some() {
-                println!("Note released {:?}", key.to_note());
-                let just_notes: Vec<_> = voices.iter().map(|v| v.0.item).collect();
-                println!("{:?}", just_notes);
+                println!("Key released: {:?}, voices: {:?}", key, voices);
             }
         }
         _ => (),
---
+++ src/voices/voices.rs
@@ -0,0 +1,96 @@
+use std::{
+    cmp::Ordering,
+    collections::BTreeMap,
+    fmt::{write, Debug},
+    time::SystemTime,
+};
+
+use rdev::Key;
+use rodio::Sink;
+
+#[derive(Debug, Clone, Copy, Eq)]
+struct WithCreationTime<T> {
+    item: T,
+    creation_time: SystemTime,
+}
+
+impl<T> WithCreationTime<T> {
+    fn new(item: T) -> Self {
+        Self {
+            item,
+            creation_time: SystemTime::now(),
+        }
+    }
+}
+
+impl<T> PartialEq for WithCreationTime<T>
+where
+    T: PartialEq,
+{
+    fn eq(&self, other: &Self) -> bool {
+        self.item.eq(&other.item)
+    }
+}
+
+impl<T> PartialOrd for WithCreationTime<T>
+where
+    T: PartialEq + Eq,
+{
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        if self.item == other.item {
+            Some(Ordering::Equal)
+        } else {
+            self.creation_time.partial_cmp(&other.creation_time)
+        }
+    }
+}
+
+impl<T> Ord for WithCreationTime<T>
+where
+    T: PartialEq + Eq,
+{
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        if self.item == other.item {
+            Ordering::Equal
+        } else {
+            self.creation_time.cmp(&other.creation_time)
+        }
+    }
+}
+
+pub struct Voices {
+    voices: BTreeMap<WithCreationTime<Key>, Sink>,
+    capacity: usize,
+}
+
+impl Voices {
+    pub fn new(capacity: usize) -> Self {
+        Self {
+            voices: BTreeMap::new(),
+            capacity,
+        }
+    }
+
+    pub fn contains(&self, key: &Key) -> bool {
+        self.voices.contains_key(&WithCreationTime::new(*key))
+    }
+
+    pub fn insert(&mut self, key: Key, sink: Sink) -> Option<Sink> {
+        if self.voices.len() == self.capacity {
+            self.voices.pop_first();
+        }
+        self.voices.insert(WithCreationTime::new(key), sink)
+    }
+
+    pub fn remove(&mut self, key: &Key) -> Option<Sink> {
+        self.voices.remove(&WithCreationTime::new(*key))
+    }
+}
+
+impl Debug for Voices {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let keys: Vec<_> = self.voices.iter().map(|v| v.0.item).collect();
+
+        write!(f, "capacity: {:?}, keys: {:?}", self.capacity, keys)
+    }
+}
